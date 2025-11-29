e53b2b1c026e8845990639f3d4abbc57e37b55fb
bluelawson <plus.cron@gmail.com>
2025-11-29 11:21:08 +0900

    開発用の動線を整備

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import { Button } from "@/components/shadcn/button";
+import Link from "next/link";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function TemporaryPageLink({ children }: Props) {
+  return (
+    <div className="px-3 py-2 flex items-center  bg-slate-300 rounded cursor-pointer hover:bg-slate-400">
+      {children}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div>
-      <Button>送信</Button>
-    </div>
+    <>
+      <div className="mx-24 my-12 px-6 py-6 h-[800px] space-x-3 flex items-start bg-slate-100">
+        <TemporaryPageLink>
+          <Link href={"/review/register"}>レビュー記事登録</Link>
+        </TemporaryPageLink>
+      </div>
+    </>
   );
 }
---
+++ src/app/review/register/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReviewDetail = () => {
+  return <div>page</div>;
+};
+
+export default ReviewDetail;
